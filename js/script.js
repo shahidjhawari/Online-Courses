9603006d4159d596bbc538152c9f785ea9656173
Shahid Iqbal <130569081+shahidjhawari@users.noreply.github.com>
2023-11-14 10:52:13 +0500

    add scroll page with progress bar

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Progress Bar with Scroll Page
+window.onscroll = function(){
+    var pos = document.documentElement.scrollTop;
+    var calc_height = document.documentElement.scrollHeight-document.documentElement.clientHeight;
+    var scroll = pos * 100 / calc_height;
+    document.getElementById("progress").style.width = scroll + "%" ;
+}
