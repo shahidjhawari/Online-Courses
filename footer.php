82bc096649f20cbe6d4baf3e54a9cbe4fb1173a9
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-11-19 18:45:38 +0500

    top, footer files separate php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!--Footer Section Start-->
+<footer class="bg-warning text-dark text-center py-3">
+      <div class="container">
+          &copy; <b>2023 NAWAB ACADEMY. All rights reserved.</b>
+      </div>
+  </footer>
+  <!--Footer Section End-->
+
+    <!--Java Script Files Here Start-->
+    <script src="js/script.js"></script>
+</body>
+</html>
