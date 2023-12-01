1ae69d30fdfca083cd188c17972cadeebed49a7b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-12-01 10:26:30 +0500

    minor changes

--- top.php
+++ top.php
@@ -9,8 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NAWAB ACADEMY</title>
     <!--Bootstrap files Here-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" href="bootstrap/bootstrap.bundle.js">
     <link rel="stylesheet" href="bootstrap/bootstrap.css">
