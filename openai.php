adba706e35ffd10b29dc02cc2625fb71b6424ad7
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2023-12-08 10:49:37 +0500

    add bootstrap cdn link instead of offline

--- openai.php
+++ openai.php
@@ -8,12 +8,11 @@
     <meta name="author" content="MAHAR SHAHID IQBAL JHAWARI">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NAWAB ACADEMY</title>
+    <!--Font Awesome files Here-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!--Bootstrap files Here-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="bootstrap/bootstrap.bundle.js">
-    <link rel="stylesheet" href="bootstrap/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <!--Own files Here-->
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/responsive.css">
